3a2b6c3453804e9689ccf3b1024d24c0e727f2c1
DmytroKarpyuk <52283766+DmytroKarpyuk@users.noreply.github.com>
2020-10-28 20:33:56 +0100

    Edited TS_Script_info, PhotosEdit.ts

--- HataCom/TS_Scripts/Photos/PhotosDomElements.ts
+++ HataCom/TS_Scripts/Photos/PhotosDomElements.ts
@@ -4,11 +4,15 @@
     addBtn: HTMLDivElement;
     editBtn: HTMLDivElement;
     removeBtn: HTMLDivElement;
+    popUp: HTMLDivElement;
+    popUpCancel: HTMLDivElement;
 
     constructor() {
         this.mainContainer = document.querySelector('.main-container') as HTMLDivElement;
         this.addBtn = document.querySelector('.add-photo-btn') as HTMLDivElement;
         this.editBtn = document.querySelector('.edit-album-btn') as HTMLDivElement;
         this.removeBtn = document.querySelector('.remove-photo-btn') as HTMLDivElement;
+        this.popUp = document.querySelector('.pop-up') as HTMLDivElement;
+        this.popUpCancel = document.querySelector('.pop-up-cancel') as HTMLDivElement;
     }
 } 
--- HataCom/TS_Scripts/Photos/PhotosEdit.ts
+++ HataCom/TS_Scripts/Photos/PhotosEdit.ts
@@ -1,3 +1,5 @@
+import { PhotosDomElements } from './PhotosDomElements';
+
 export class PhotosEdit {
 
     constructor() {
@@ -6,33 +8,23 @@ export class PhotosEdit {
 
     init(): void {
         document.addEventListener('DOMContentLoaded', () => {
-            const editAlbumBtn = document.querySelector('.edit-album-btn') as HTMLDivElement;
-            editAlbumBtn.addEventListener('click', this.editStart);
-
-            const popUpCancel = document.querySelector('.pop-up-cancel') as HTMLDivElement;
-            popUpCancel.addEventListener('click', this.editCancel);
+            const photosDomElements = new PhotosDomElements();
+            photosDomElements.editBtn.addEventListener('click', this.editStart);
+            photosDomElements.popUpCancel.addEventListener('click', this.editCancel);
         });
     }
 
-    editStart(): void {
-        const popUp = document.querySelector('.pop-up') as HTMLDivElement;
-        popUp.style.marginTop = '55px';
-
-        const addPhotoBtn = document.querySelector('.add-photo-btn') as HTMLDivElement;
-        addPhotoBtn.style.display = 'none';
-
-        const removePhotoBtn = document.querySelector('.remove-photo-btn') as HTMLDivElement;
-        removePhotoBtn.style.display = 'block';
+    editStart(): void { 
+        const photosDomElements = new PhotosDomElements();
+        photosDomElements.popUp.style.marginTop = '55px';
+        photosDomElements.addBtn.style.display = 'none';
+        photosDomElements.removeBtn.style.display = 'block';
     }
 
     editCancel(): void {
-        const popUp = document.querySelector('.pop-up') as HTMLDivElement;
-        popUp.style.marginTop = '-55px';
-
-        const addPhotoBtn = document.querySelector('.add-photo-btn') as HTMLDivElement;
-        addPhotoBtn.style.display = 'block';
-
-        const removePhotoBtn = document.querySelector('.remove-photo-btn') as HTMLDivElement;
-        removePhotoBtn.style.display = 'none';
+        const photosDomElements = new PhotosDomElements();
+        photosDomElements.popUp.style.marginTop = '-55px';
+        photosDomElements.addBtn.style.display = 'block';
+        photosDomElements.removeBtn.style.display = 'none';
     }
 }
